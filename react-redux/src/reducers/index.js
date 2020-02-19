f38a3a07079527bf1d6084d81034dfa8d24cf5b1
richknicks <rlynch@gmail.com>
2020-02-19 17:39:13 -0500

    created reducer

--- react-redux/src/reducers/index.js
+++ react-redux/src/reducers/index.js
@@ -1,17 +1,36 @@
+import { FETCH_JOKES_START, FETCH_JOKES_SUCCESS, FETCH_JOKES_FAIL } from '../action';
+
+
+
 const initialState = {
      
-        id:"",
-        type:"",
-        setup:"",
-        punchline:""
+    jokes: [],
+    error: '',
+    isFetching: false
     
 };
 
 export const reducer = (state = initialState, action)=>{
     console.log("I am the action from reducer", action)
     switch(action.type){
-        case 'ADD':
-
+        case 'FETCH_JOKES_START':
+        return {
+            ...state,
+            isFetching: true,
+            error:''
+        };
+        case 'FETCH_JOKES_SUCCESS':
+            return{
+                ...state,
+                jokes: action.payload,
+                isFetching: false,
+                error:''
+            };
+        case 'FETCH_JOKES_FAIL':
+            return {
+                ...state,
+                error: action.payload
+            }
         default: 
         return state
     }
